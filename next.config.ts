5b149fd733b2dcae5ef9610dd2bf375d5a337424
Oliver Joseph Ronco-Oates <oliverronco@Olivers-MacBook-Pro.local>
2026-02-09 14:59:41 +0000

    Redirect root to /topics

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      { source: "/", destination: "/topics", permanent: false },
+    ];
+  },
 };
+
 
 export default nextConfig;
