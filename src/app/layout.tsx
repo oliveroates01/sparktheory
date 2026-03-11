75f402cba0c975d66d54f763df6f315076d021cd
Oliver Joseph Ronco-Oates <oliverronco@Olivers-MacBook-Pro.local>
2026-03-11 14:52:54 +0000

    Fix Microsoft Clarity project ID in global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "vo6b8Oy5yf");
+              })(window, document, "clarity", "script", "vo6b80y5yf");
             `,
           }}
         />
